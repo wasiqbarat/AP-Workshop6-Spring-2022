6153910c2c32e3cc760536be3695cd413c71a5d9
Wasiq Barat <wasiqlilium1399@gmail.com>
2022-04-14 15:18:19 +0430

    completed

--- Main.java
+++ Main.java
@@ -5,18 +5,23 @@ public class Main {
         ArrayList<Animal> animals = new ArrayList<>();
 
         // instantiate animals
-        Mammal girafe = new Girafe("girage", 50, 30, "it is very tall");
+        Mammal girafe = new Girafe("girafe", 50, 30, "it is very tall");
         Bird parrot = new Parrot("parrot", 10, 50, "it is very beautiful");
         Cheetah cheetah = new Cheetah("cheetah", 2, 100, "it is very fast");
         Eagle eagle = new Eagle("eagel", 1, 200, "powerful and alpha");
 
+        //adding to animal list
         animals.add(girafe);
         animals.add(parrot);
         animals.add(cheetah);
         animals.add(eagle);
 
+        // invoking show method
+        int row = 1;
         for (Animal animal : animals) {
+            System.out.print(row + ".");
             animal.show();
+            row++;
         }
 
     }
